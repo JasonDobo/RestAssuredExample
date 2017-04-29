b7f99aee20c5e207f729278c5e7f16756c511b18
JasonDobo <jason.dobo@gmail.com>
2017-04-29 15:41:31 +0100

    First ever rest assured test case

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # RestAssuredExample
+
+## Instructions:
+This project was developed using intellij and maven, on MAC
+- To run the project unzip the file into a directory
+- Navigate to to the directory using terminal and run the following command
+"mvn package -U"
+
+## Test Scenarios:
+General:
+Each test will verify the response code and JSON schema
+testEndpointIsAvailableWithNoInputs:
+- End point returns is available
+testInvalidInputString:
+- End point returns a error, when no arguments are given
+testValidZeroInput:
+- End point returns 0 for a 0 input
+testSimpleSubtraction:
+- Subtracts one number form another
+testSimpleAddition:
+- Adds one number of another
+testSimpleMultiplication:
+- Multiples one number to another
+testSimpleDivide:
+- Divides one number form another
+NOTE: I could not get this test to work, I assume as I've not tested end points before that I am passing in a invalid argument for "divide"
+testNoCalculation:
+- If no modifier is passed in, the first number in the array is returned
+NOTE: I assumed the result based on what the end point does now, but I would talk to the project owner to clarify the expected behaviour
+testThirdArgumentIsIgnored:
+- Confirm only first 2 numbers are used, again behaviour would have to confirmed with product owner
+NOTE: If I had more time I would repeat this test would more arguments and different calculations
+testFractionInputsOnFirstNumber:
+- Tests that a 500 error is returned if first number is a fraction
+testFractionInputsOnLastNumber:
+- Tests that a 500 error is returned if last number is a fraction
+NOTE: This tests fails, as the behaviour is different ot other fractions, would need to confirm with product owner
+testComplexEquationsAreNotSupported:
+- Confirms complex calculations are not supports
+
+Missing Tests:
+One: Test maths formula's with more than one calculation i.e. "* (− 5 6) 7", assuming it is supported or verify if it is not
+Two: If I had more time I would repeat "testThirdArgumentIsIgnored" with more arguments and different calculations
+
+Failing Tests:
+Currently 2 tests fail, as I am unsure as to the expected acceptance criteria of the API, these will need to be updated
---
+++ src/test/java/NormalPolishNotationTest.java
@@ -0,0 +1,140 @@
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.Response;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class NormalPolishNotationTest {
+
+    final String username = "home";
+    final String password = "test";
+    final String endPoint = "http://" + username + ":" + password + "@home-assignment.herokuapp.com/api/calculate/";
+
+    @Test
+    public void testEndpointIsAvailableWithNoInputs() {
+        given().when().get(endPoint).then().statusCode(200);
+    }
+
+    @Test
+    public void testInvalidInputString() {
+        String input = "string";
+        String testEndPoint = endPoint + input;
+        get500Response(testEndPoint);
+    }
+
+    @Test
+    public void testValidZeroInput() {
+        String input = "0";
+        String testEndPoint = endPoint + input;
+
+        getResponse(testEndPoint, input, Integer.valueOf(input));
+    }
+
+    @Test
+    public void testSimpleSubtraction() {
+        String input = "− 5 6";
+        String testEndPoint = endPoint + input;
+        int result = 11;
+
+        getResponse(testEndPoint, input, result);
+    }
+
+    @Test
+    public void testSimpleAddition() {
+        String input = "+ 1 3";
+        String testEndPoint = endPoint + input;
+        int result = 4;
+
+        getResponse(testEndPoint, input,  result);
+    }
+
+    @Test
+    public void testSimpleMultiplication() {
+        String input = "* 6 7";
+        String testEndPoint = endPoint + input;
+        int result = 42;
+
+        getResponse(testEndPoint, input, result);
+    }
+
+    @Test
+    public void testSimpleDivide() {
+        String input = "/ 20 5";
+        String testEndPoint = endPoint + input;
+        int result = 4;
+
+        getResponse(testEndPoint, input,  result);
+    }
+
+    @Test
+    public void testNoCalculation() {
+        System.out.println("The instruction page does no clarify this acceptance criteria");
+        String input = "20 5";
+        String testEndPoint = endPoint + input;
+        int result = 20;
+
+        getResponse(testEndPoint, input,  result);
+    }
+
+    @Test
+    public void testThirdArgumentIsIgnored() {
+        String input = "+ 1 3 5";
+        String testEndPoint = endPoint + input;
+        int result = 4;
+
+        getResponse(testEndPoint, input,  result);
+
+    }
+
+    @Test
+    public void testFractionInputsOnFirstNumber() {
+        String input = "+ 1.5 3";
+        String testEndPoint = endPoint + input;
+
+        get500Response(testEndPoint);
+    }
+
+    @Test
+    public void testFractionInputsOnLastNumber() {
+        String input = "+ 1 3.5";
+        String testEndPoint = endPoint + input;
+
+        get500Response(testEndPoint);
+    }
+
+    @Test
+    public void testComplexEquationsAreNotSupported() {
+        String input = "* (− 7 4) 7";
+        String testEndPoint = endPoint + input;
+
+        get500Response(testEndPoint);
+    }
+
+    private void get500Response(String testEndPoint) {
+        given().
+                when().
+                get(testEndPoint).
+                then().
+                statusCode(500);
+    }
+
+
+    private void getResponse(String testEndPoint, String expression, int result) {
+        Response response = given().
+                    when().
+                            get(testEndPoint).
+                    then().
+                            statusCode(200).
+                            contentType(ContentType.JSON).
+                            body(matchesJsonSchemaInClasspath("tax-calculator-schema.json")).
+                            body("expression", equalTo(expression)).
+                            body("result", equalTo(result)).
+                            extract().
+                            response();
+
+        System.out.println("JSON Response: " + response.asString());
+    }
+}
